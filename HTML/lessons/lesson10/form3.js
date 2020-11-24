ba0071dab757dbf8b9da69d90807f01cb5d3cb1f
Leonid <tishkevich.leonid@gmail.com>
2020-11-24 09:36:10 +0300

    Added sel-study dir

--- HTML/lessons/lesson10/form3.js
+++ HTML/lessons/lesson10/form3.js
@@ -1,13 +1,11 @@
+function checkForm() {
+  let p = document.getElementsByTagName("p");
+  let c = document.forms[0].cl.value;
+  let s = document.forms[0].sz.value;
 
-
-function checkForm(){
-	let p=document.getElementsByTagName('p');
-    let c=document.forms[0].cl.value;
-    let s=document.forms[0].sz.value;
-  
-for(let i=0; i<p.length; i++){
-p[i].style.color=c;
-p[i].style.fontSize=s;
+  for (let i = 0; i < p.length; i++) {
+    p[i].style.color = c;
+    p[i].style.fontSize = `${s}px`;
+  }
 }
-}
-let t=setInterval(checkForm,50);
+let t = setInterval(checkForm, 50);
